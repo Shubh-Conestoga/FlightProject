e016eaff9c7a37b6606d3bc5d290dc6071d5c5ed
Shubh-Conestoga <Spatel8035@conestogac.on.ca>
2021-12-10 19:37:39 +0530

    validaiton Added!

---
+++ FinalProjectByFinal-5/FinalProjectByFinal-5/AgeConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FinalProjectByFinal_5
+{
+    public class AgeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int age = int.Parse(value.ToString());
+            if (age < 18)
+            {
+                return Brushes.Yellow;
+            }
+            else
+            {
+                return Brushes.White;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FinalProjectByFinal-5/FinalProjectByFinal-5/BookingValidator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace FinalProjectByFinal_5
+{
+    class BookingValidator : ValidationRule
+    {
+        private string validationFor;
+
+        public string ValidationFor { get => validationFor; set => validationFor = value; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (validationFor == "name")
+            {
+                string name = value.ToString();
+                if (name.Length > 0)
+                {
+                    return ValidationResult.ValidResult;
+                }
+                else
+                {
+                    return new ValidationResult(false, "Please Enter Name");
+                }
+            }
+            if (validationFor == "passport")
+            {
+                string passport = value.ToString();
+                if (passport.Trim().Length != 6)
+                {
+                    return new ValidationResult(false, "Passport should have 6 letters");
+                }
+                else
+                {
+                    return ValidationResult.ValidResult;
+                }
+            }
+            if (validationFor == "creditcard")
+            {
+                string creditcard = value.ToString();
+                long cc;
+                if (creditcard.Trim().Length != 16)
+                {
+                    return new ValidationResult(false, "Creditcard should have 16 digits!");
+                }
+                else if (!long.TryParse(creditcard.Trim(), out cc))
+                {
+                    return new ValidationResult(false,"Creditcard number must only contains 16 digits");
+                }
+                else
+                {
+                    return ValidationResult.ValidResult;
+                }
+            }
+            if (validationFor == "age")
+            {
+                string age = value.ToString().Trim();
+                int ageVal;
+                long cc;
+                if (age.Length == 0)
+                {
+                    return new ValidationResult(false, "Please Enter Your Age");
+                }
+                else if (!int.TryParse(age, out ageVal))
+                {
+                    return new ValidationResult(false, "Age must be an integer value");
+                }
+                else if (ageVal <= 0)
+                { 
+                    return new ValidationResult(false, "Age must be greater than zero!");
+                }
+                else
+                {
+                    return ValidationResult.ValidResult;
+                }
+            }
+            return new ValidationResult(false,"Something went wrong");
+        }
+    }
+}
